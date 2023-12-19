4a688d1aa527f9a80714a862993119ffcdbf2fd2
Hugo U. R. Strand <hugo.strand@gmail.com>
2023-12-19 13:09:30 +0100

    [diag] generalize to ks fixed verts

--- python/triqs_soehyb/diag.py
+++ python/triqs_soehyb/diag.py
@@ -7,6 +7,8 @@ from copy import copy
 
 from functools import reduce
 
+import numpy as np
+
 
 def flatten_pairing_to_list(pairing):
     return reduce(lambda x, y: list(x) + list(y), pairing)
@@ -41,9 +43,9 @@ def all_pairings(order):
     yield from _recursive_pairing(1, [], unpaired)
 
 
-def all_connected_pairings(order, k=1):
+def all_connected_pairings(order, ks=[1]):
     for parity, pairing in all_pairings(order):
-        if is_connected(pairing, k=k):
+        if is_connected(pairing, ks=ks):
             yield parity, pairing
 
 
@@ -55,25 +57,39 @@ def is_crossing(p1, p2):
         (p2[0] < p1[0] and p1[0] < p2[1] and p2[1] < p1[1])
 
 
-def is_k_connector(pair, k):
-    k = k - 1
-    return (pair[0] <= k and pair[1] > k) or (pair[1] <= k and pair[0] > k)
+def is_valid_ks(ks):
+    ks = np.asarray(ks)
+    return (ks >= 0).all() and  (np.diff(ks) >= 0).all()
 
 
-def split_pairing(pairing, k):
+def interval(vertex, ks):
+    assert( is_valid_ks(ks) )
+    for interval, k in enumerate(ks):
+        if vertex <= k - 1:
+            return interval
+    return interval + 1
+
+
+def is_ks_connector(pair, ks):
+    #k = k - 1
+    #return (pair[0] <= k and pair[1] > k) or (pair[1] <= k and pair[0] > k)
+    return interval(pair[0], ks=ks) != interval(pair[1], ks=ks)
+
+
+def split_pairing(pairing, ks):
     rest, connectors = [], []
     for pair in pairing:
-        if is_k_connector(pair, k):
+        if is_ks_connector(pair, ks=ks):
             connectors.append(pair)
         else:
             rest.append(pair)
     return connectors, rest
 
 
-def is_connected(pairing, k=1):
+def is_connected(pairing, ks):
 
     connected = []
-    stack, disconnected = split_pairing(pairing, k=k)
+    stack, disconnected = split_pairing(pairing, ks)
     
     #pairing = copy(pairing)
     #stack = [pairing.pop(0)]
@@ -92,6 +108,7 @@ def is_connected(pairing, k=1):
 
 if __name__ == '__main__':
 
+    #for order in [1]:
     for order in [1, 2, 3, 4]:
         print('-'*72)
         print(f'order = {order}')
@@ -99,73 +116,133 @@ if __name__ == '__main__':
         for par, pair in all_connected_pairings(order):
             print(f'{par:+d}, {pair}')
 
-
-    def plot_pairing(pairing):
-
-        n = 2*len(pairing)
-        
-    import numpy as np    
+    #exit()
+                    
     import matplotlib.pyplot as plt
-
     from matplotlib.patches import Arc
-
     
     def plot_pairs(ax, pairs, ks=[]):
 
         colors = dict()
-        for k in ks:
-            c = ax.plot([], [])[0].get_color()
-            colors[k] = c
+        
+        #for k in ks:
+        for i1 in range(len(ks)+1):
+            for i2 in range(len(ks)+1):
+                if i1 != i2:
+                    c = ax.plot([], [])[0].get_color()
+                else:
+                    c = 'gray'
+                colors[(i1, i2)] = c
         
         # arcs
         for p in pairs:
             r = p[1] - p[0]
             c = 0.5*np.sum(p)
-            color = 'gray'
-            for k in ks:
-                if is_k_connector(p, k=k):
-                    color = colors[k]
+            #color = 'gray'
+            #for k in ks:
+            #    if is_ks_connector(p, ks=[k]):
+            #        color = colors[k]
+            color = colors[(interval(p[0], ks), interval(p[1], ks))]
                     
             a = Arc((c, 0), r, r, theta1=0, theta2=180, fill=False, color=color)
             ax.add_patch(a)
 
         # backbone
-        n = 2*len(pairs) - 1
-        ax.plot([0, n], [0, 0], 'k', lw=2)
+        n = np.max([2*len(pairs) - 1, np.max(ks)])
+        s = np.min([0, np.min(ks) - 1])
+        
+        ax.plot([s, n], [0, 0], 'k', lw=2)
 
         # dashed vertical lines
+        x = -1.
+        y = 0.
         for k in ks:
-            x = k - 0.5
-            ax.plot([x], [0], '.r')
+            
+            x_new = k - 0.5
+            if x_new == x:
+                y -= 0.25
+            else:
+                y = 0.
+            x = x_new
+                
+            ax.plot([x], [y], '.r')
 
         ax.tick_params(bottom=False, top=False, left=False, right=False,
                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
         ax.spines[:].set_visible(False)
-        ax.set_ylim([-0.5, (n+1)//2 - 0.25])
+        ax.set_ylim([-0.5*(1+len(ks)), (n+1)//2 - 0.25])
 
-    order = 1
-    nks = { 1:1, 2:2, 3:7, 4:42 }
 
-    nk = nks[order]
-    nr = 2*order - 1
-    subp = [nr, nk, 0]
+    if True:
+        order = 2
+        nks = { 1:1, 2:2, 3:7, 4:42 }
 
-    fig = plt.figure(figsize=(26 * nk/42, 4 * nr/7))
+        nk = nks[order]
+        nr = 2*order - 1
+        subp = [nr, nk, 0]
 
-    for k in range(1, 2*order):
-        
-        parity_pairs = [ x for x in all_connected_pairings(order, k=k) ]
-        n = len(parity_pairs)
-        print(f'n = {n}')
+        fig = plt.figure(figsize=(26 * nk/42, 4 * nr/7))
 
-        for par, pairs in parity_pairs:
-            subp[-1] += 1
-            ax = fig.add_subplot(*subp, aspect='equal')
-            plot_pairs(ax, pairs, ks=[k])
+        for k in range(1, 2*order):
 
-        subp[-1] = nk * k
-        
-    plt.tight_layout()
-    plt.savefig(f'figure_order_{order}.pdf')
-    plt.show()
-    
+            parity_pairs = [ x for x in all_connected_pairings(order, ks=[k]) ]
+            n = len(parity_pairs)
+            print(f'n = {n}')
+
+            for par, pairs in parity_pairs:
+                subp[-1] += 1
+                ax = fig.add_subplot(*subp, aspect='equal')
+                plot_pairs(ax, pairs, ks=[k])
+
+            subp[-1] = nk * k
+
+        plt.tight_layout()
+        plt.savefig(f'figure_order_{order}.pdf')
+        plt.show()
+        exit()
+
+
+    if False:
+        subp = [4, 5, 0]
+        fig = plt.figure(figsize=(10, 10))
+
+        order = 2
+        for k1 in range(0, 2*order):
+            for k2 in range(k1, 2*order+1):
+                print(f'k1, k2 = {k1}, {k2}')
+
+                parity_pairs = [ x for x in all_connected_pairings(order, ks=[k1, k2]) ]
+
+                for par, pairs in parity_pairs:
+                    subp[-1] += 1
+                    ax = fig.add_subplot(*subp, aspect='equal')
+                    plot_pairs(ax, pairs, ks=[k1, k2])
+
+                #for parity, pairs in all_connected_pairings(order, ks=[k1, k2]):
+                #    print(f'{parity:+d}, {pairs}')
+
+        plt.tight_layout()
+        plt.show()
+        exit()
+
+    if False:
+        subp = [10, 10, 0]
+        fig = plt.figure(figsize=(10, 10))
+
+        order = 2
+        for k1 in range(0, 2*order):
+            for k2 in range(k1, 2*order+1):
+                for k3 in range(k2, 2*order+1):
+                    print(f'k1, k2, k3 = {k1}, {k2}, {k3}')
+                    ks = [k1, k2, k3]
+                    parity_pairs = [ x for x in all_connected_pairings(order, ks=ks) ]
+
+                    for par, pairs in parity_pairs:
+                        subp[-1] += 1
+                        ax = fig.add_subplot(*subp, aspect='equal')
+                        plot_pairs(ax, pairs, ks=ks)
+
+        plt.tight_layout()
+        plt.show()
+        exit()
+
