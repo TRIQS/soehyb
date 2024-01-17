df5f27c8647293fd79b4b2f31a960582056c09b5
Hertz4 <88074238+Hertz4@users.noreply.github.com>
2024-01-18 02:08:43 +0800

    Simplify group functions

--- c++/triqs_soehyb/impurity.cpp
+++ c++/triqs_soehyb/impurity.cpp
@@ -81,9 +81,10 @@ void fastdiagram::hyb_decomposition(bool poledlrflag){
 int fastdiagram::number_of_diagrams(int m){
     return pow(P,m-1) * pow(2,m-1);
 }
-nda::array<dcomplex,3> fastdiagram::Sigma_calc_group(nda::array<dcomplex,3> Gt, nda::array<int,2> D, nda::array<int,1> diagramindex, int num_diagram_per_fb, int N){
+nda::array<dcomplex,3> fastdiagram::Sigma_calc_group(nda::array<dcomplex,3> Gt, nda::array<int,2> D, nda::array<int,1> diagramindex,int N){
     int Nd = diagramindex.shape(0);
     int m = D.shape(0);
+    int num_diagram_per_fb = pow(P,m-1); 
     auto Diagram = nda::array<dcomplex,3>(r,N,N);
     for (int id = 0; id<Nd; ++id){
         auto fb = nda::vector<int>(m); //utility for iteration
@@ -112,7 +113,7 @@ nda::array<dcomplex,3> fastdiagram::Sigma_calc(nda::array<dcomplex,3> Gt, std::s
     int m = D_OCA.shape(0);
     int Num_diagram_oca = this->number_of_diagrams(m);
     std::cout << "number of OCA diagrams = " << Num_diagram_oca <<"\n";
-    nda::array<dcomplex,3> Sigma_OCA = - Sigma_calc_group(Gt, D_OCA, arange(Num_diagram_oca), pow(P,m-1),  N);
+    nda::array<dcomplex,3> Sigma_OCA = - Sigma_calc_group(Gt, D_OCA, arange(Num_diagram_oca),  N);
     // nda::array<dcomplex,3> Sigma_OCA = - Sigma_Diagram_calc_sum_all(Delta_F, Delta_F_reflect, D_OCA,  Deltat, Deltat_reflect,Gt, itops,  beta,  F,  F_dag);
     std::cout << "S-OCA: done\n";
     if (order.compare("OCA")==0) return make_regular(Sigma_NCA+Sigma_OCA);
@@ -128,10 +129,11 @@ nda::array<dcomplex,3> fastdiagram::Sigma_calc(nda::array<dcomplex,3> Gt, std::s
     return make_regular(Sigma_NCA+Sigma_OCA+Sigma_TCA);
 }
 
-nda::array<dcomplex,3> fastdiagram::G_calc_group(nda::array<dcomplex,3> Gt, nda::array<int,2> D, nda::array<int,1> diagramindex, int num_diagram_per_fb, int N){
+nda::array<dcomplex,3> fastdiagram::G_calc_group(nda::array<dcomplex,3> Gt, nda::array<int,2> D, nda::array<int,1> diagramindex, int N){
     int Nd = diagramindex.shape(0);
     int m = D.shape(0);
     auto Diagram = nda::array<dcomplex,3>(r,n,n);
+    int num_diagram_per_fb = pow(P,m-1); 
     for (int id = 0; id<Nd; ++id){
         auto fb = nda::vector<int>(m); //utility for iteration
         int num = diagramindex(id);
@@ -159,7 +161,7 @@ nda::array<dcomplex,3> fastdiagram::G_calc(nda::array<dcomplex,3> Gt, std::strin
     int m = D_OCA.shape(0);
     int Num_diagram_oca = this->number_of_diagrams(m);
     std::cout << "number of OCA diagrams = " << Num_diagram_oca <<"\n";
-    nda::array<dcomplex,3> g_OCA = - G_calc_group(Gt, D_OCA, arange(Num_diagram_oca), pow(P,m-1),  N);
+    nda::array<dcomplex,3> g_OCA = - G_calc_group(Gt, D_OCA, arange(Num_diagram_oca),  N);
 
     // nda::array<dcomplex,3> g_OCA = -G_Diagram_calc_sum_all(Delta_F, Delta_F_reflect, D_OCA,  Deltat, Deltat_reflect,Gt, itops,  beta,  F,  F_dag); 
     std::cout << "G-OCA done\n";
