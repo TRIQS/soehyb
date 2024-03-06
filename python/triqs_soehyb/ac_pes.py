1d1a8225cbbef2a5ce6d0add2075b199560c0317
Hertz4 <88074238+Hertz4@users.noreply.github.com>
2024-03-06 13:44:42 -0600

    add Hermitian=False choices

--- python/triqs_soehyb/ac_pes.py
+++ python/triqs_soehyb/ac_pes.py
@@ -17,7 +17,7 @@ def eval_with_pole(pol, Z, weight):
         G = M@np.reshape(weight, (weight.shape[0], weight.shape[1]*weight.shape[2]))
         return np.reshape(G, (G.shape[0],  weight.shape[1], weight.shape[2]))
 
-def get_weight(pol, Z, G, cleanflag=True, maxiter=100000):
+def get_weight(pol, Z, G, cleanflag=True, maxiter=100000, Hermitian = True):
     pol_t = np.reshape(pol,[pol.size,1])
     M = 1/(Z-pol_t)
     M = M.transpose()
@@ -34,18 +34,25 @@ def get_weight(pol, Z, G, cleanflag=True, maxiter=100000):
         Norb = G.shape[1]
         R = np.zeros((Np, Norb, Norb), dtype = np.complex128)
         if cleanflag == True:
-            for i in range(Norb):
-                GG = np.concatenate([G[:,i,i].real,G[:,i,i].imag])
-                R[:,i,i] = np.linalg.lstsq(MM, GG,rcond=0)[0]
-                for j in range(i+1,Norb):
-                    g1 = (G[:,j,i] + G[:,i,j])/2.0
-                    g2 = (G[:,i,j] - G[:,j,i])/2.0 
-                    GG1 = np.concatenate([g1.real,g1.imag])
-                    GG2 = np.concatenate([g2.imag, -g2.real])
-                    R1 = np.linalg.lstsq(MM, GG1,rcond=0)[0]
-                    R2 = np.linalg.lstsq(MM, GG2,rcond=0)[0]
-                    R[:,i,j] = R1 + 1j*R2
-                    R[:,j,i] = R1 - 1j*R2
+            if Hermitian == False:
+                for i in range(Norb):
+                    for j in range(Norb):
+                        
+                        R[:,i,j] = np.linalg.lstsq(M, G[:,i,j],rcond=0)[0]
+                #not implemented
+            else:
+                for i in range(Norb):
+                    GG = np.concatenate([G[:,i,i].real,G[:,i,i].imag])
+                    R[:,i,i] = np.linalg.lstsq(MM, GG,rcond=0)[0]
+                    for j in range(i+1,Norb):
+                        g1 = (G[:,j,i] + G[:,i,j])/2.0
+                        g2 = (G[:,i,j] - G[:,j,i])/2.0 
+                        GG1 = np.concatenate([g1.real,g1.imag])
+                        GG2 = np.concatenate([g2.imag, -g2.real])
+                        R1 = np.linalg.lstsq(MM, GG1,rcond=0)[0]
+                        R2 = np.linalg.lstsq(MM, GG2,rcond=0)[0]
+                        R[:,i,j] = R1 + 1j*R2
+                        R[:,j,i] = R1 - 1j*R2
         else:
             X = [cp.Variable((Norb, Norb), hermitian=True) for i in range(Np) ]
             Nw = len(Z)
@@ -69,8 +76,9 @@ def get_weight(pol, Z, G, cleanflag=True, maxiter=100000):
                 
        
         residue = 1.0*G
-        for i in range(Np):
-            residue = residue -  M[:,i,None,None] * R[i]
+        for i in range(Norb):
+            for j in range(Norb):
+                residue[:,i,j] = residue[:,i,j] -  M@ R[:,i,j]
     return R, M, residue
     
     
@@ -82,8 +90,8 @@ def aaa_reduce(pol, R, eps=1e-6):
     nonz_index = Rnorm>eps
     return pol[nonz_index], R[nonz_index]
 
-def erroreval(pol,  Z, G, cleanflag=True, maxiter=100000):
-    R, M, residue = get_weight(pol,  Z, G, cleanflag=cleanflag, maxiter=maxiter)
+def erroreval(pol,  Z, G, cleanflag=True, maxiter=100000,Hermitian=True):
+    R, M, residue = get_weight(pol,  Z, G, cleanflag=cleanflag, maxiter=maxiter,Hermitian=Hermitian)
     if len(G.shape)==1:
         y = np.linalg.norm(residue)
         grad = np.real(np.dot(np.conj(residue) ,(R*(M**2))))
@@ -97,20 +105,23 @@ def erroreval(pol,  Z, G, cleanflag=True, maxiter=100000):
             for w in range(Nw):
                 grad[k] = grad[k] + np.real(np.sum((M[w,k]**2)*(np.conj(residue[w,:,:]) * R[k])))
 
-    
     grad = -grad/y
     return y, grad
 
 
-def polefitting(Deltaiw, Z, Np_max=50,eps = 1e-5):
+def polefitting(Deltaiw, Z, Np_max=50,eps = 1e-5,Hermitian=True):
     for mmax in range(4,Np_max,2):
         r = aaa_matrix_real(Deltaiw, Z, mmax=mmax)
-        pol = np.real(r.pol())
-        weight, _, residue = get_weight(pol, Z, Deltaiw,cleanflag=True)
-        pol, weight = aaa_reduce(pol, weight,eps)
-        fhere = lambda pole: erroreval(pole,Z, Deltaiw,cleanflag=True)
+        pol = r.pol()
+        # pol = pol[np.abs(np.imag(pol))<0.1]
+        pol = np.real(pol)
+        weight, _, residue = get_weight(pol, Z, Deltaiw,cleanflag=True,Hermitian=Hermitian)
+        # pol, weight = aaa_reduce(pol, weight,eps)
+        fhere = lambda pole: erroreval(pole,Z, Deltaiw,cleanflag=True,Hermitian=Hermitian)
         res = scipy.optimize.minimize(fhere,pol, method='L-BFGS-B', jac=True,options= {"disp" :False,"gtol":1e-10,"ftol":1e-10})
-        weight, _, residue = get_weight(res.x, Z, Deltaiw,cleanflag=True)
+        weight, _, residue = get_weight(res.x, Z, Deltaiw,cleanflag=True, Hermitian=Hermitian)
+        # print(np.linalg.norm(residue))
         if np.linalg.norm(residue)<eps:
             return weight, res.x, np.linalg.norm(residue)
+        
     return weight, res.x, np.linalg.norm(residue)
