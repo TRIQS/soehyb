2903b96f82a16b61682fa3566e93e1be43eacc55
Paco Rilloraza <fpr2017@nyu.edu>
2025-07-25 15:34:48 -0400

    b-s python for atom_diag symmetry sets

--- python/triqs_soehyb/atom_diag_to_h5.py
+++ python/triqs_soehyb/atom_diag_to_h5.py
@@ -6,16 +6,22 @@ from h5 import HDFArchive
 import atom_diag_utils as utils
 
 if __name__ == "__main__":
-    # parameters to tune
-    norb = 4
-    h5_fname = "spin_flip_fermion.h5"
+    # parameters to tune 
+    norb = 2
+    h5_fname = "spin_flip_fermion_all_sym.h5"
     
     N = 0
+    N0 = 0
+    N1 = 0
     for kap in range(norb):
         for sig in ['up', 'do']:
             N += n(sig, kap)
+            if kap == 0:
+                N0 += n(sig, kap)
+            else:
+                N1 += n(sig, kap)
 
-    sym_ops = [N]
+    sym_ops = [N0, N1]
 
     # fixed parameters
     beta = 2.0
@@ -34,6 +40,33 @@ if __name__ == "__main__":
         fops += [ ('up', i)]
     ad = AtomDiag(H, fops, sym_ops)
 
+    # look at c_connection and cdag_connection of ad
+    # find groups that are the same
+    row_groups = np.zeros(2 * norb, dtype = int) 
+    # for the zeroth column, mark the rows that have the same first element
+    counter = 1 # increments every time a new group is found
+    for s in range(0, ad.n_subspaces):
+        oidx0 = 1
+        found_oidx0 = False
+        for i in range(0, 2 * norb):
+            if not found_oidx0 and row_groups[i] != i:
+                oidx0 = i
+                found_oidx0 = True
+        for oidx in range(oidx0, 2 * norb):
+            found_pair = False # gets set to True if a pair is found
+            for oidx2 in range(0, oidx): # loop over the previous rows
+                try:
+                    x = ad.c_connection(oidx, s)
+                    y = ad.c_connection(oidx2, s)
+                except Exception as e:
+                    print(f"Failed at oidx={oidx}, s = {s}: {e}")
+                if not found_pair and x == y:
+                    row_groups[oidx] = row_groups[oidx2] # set the row group to a previous one
+                    found_pair = True
+            if not found_pair: # no pairs found, so create a new group
+                row_groups[oidx] = counter
+                counter += 1
+    num_sym_sets = len(set(row_groups))
     # get Hamiltonian as a matrix
     H_mat = utils.get_full_h_atomic(ad)
     
@@ -50,10 +83,11 @@ if __name__ == "__main__":
     H_perm = H_perm.astype(np.int64)
     H_mat_perm = H_mat[H_perm][:,H_perm]
 
-    # c_blocks = [[], [], [], []]
-    # cdag_blocks = [[], [], [], []]
-    c_blocks = [[] for _ in range(ad.n_subspaces)]
-    cdag_blocks = [[] for _ in range(ad.n_subspaces)]
+    # need num_sym_sets copies of these
+    # c_blocks = [[] for _ in range(ad.n_subspaces)]
+    # cdag_blocks = [[] for _ in range(ad.n_subspaces)]
+    c_blocks = [[[] for _ in range(ad.n_subspaces)] for _ in range(num_sym_sets)]
+    cdag_blocks = [[[] for _ in range(ad.n_subspaces)] for _ in range(num_sym_sets)]
 
     # orbital indices: do 0, do 1, up 0, up 1
     # save blocks of creation and annihilation operators
@@ -61,16 +95,18 @@ if __name__ == "__main__":
         for sidx in range(ad.n_subspaces):
             cidx = ad.c_connection(oidx, sidx)
             fock_idx = np.ix_(ad.fock_states[cidx], ad.fock_states[sidx])
-            c_blocks[sidx] = c_blocks[sidx] + \
-                [utils.get_full_c_matrix(ad, oidx)[fock_idx]]
+            # c_blocks[sidx] = c_blocks[sidx] + [utils.get_full_c_matrix(ad, oidx)[fock_idx]]
+            c_blocks[row_groups[oidx]][sidx] = c_blocks[row_groups[oidx]][sidx] + [utils.get_full_c_matrix(ad, oidx)[fock_idx]]
             didx = ad.cdag_connection(oidx, sidx)
             fock_idx = np.ix_(ad.fock_states[didx], ad.fock_states[sidx])
-            cdag_blocks[sidx] = cdag_blocks[sidx] + \
-                [utils.get_full_cdag_matrix(ad, oidx)[fock_idx]]
-    for sidx in range(ad.n_subspaces):        
-        c_blocks[sidx] = np.array(c_blocks[sidx])
-        cdag_blocks[sidx] = np.array(cdag_blocks[sidx])
-    
+            # cdag_blocks[sidx] = cdag_blocks[sidx] + [utils.get_full_cdag_matrix(ad, oidx)[fock_idx]]
+            cdag_blocks[row_groups[oidx]][sidx] = cdag_blocks[row_groups[oidx]][sidx] + [utils.get_full_cdag_matrix(ad, oidx)[fock_idx]]
+
+    for gidx in range(num_sym_sets):
+        for sidx in range(ad.n_subspaces):
+            c_blocks[gidx][sidx] = np.array(c_blocks[gidx][sidx])
+            cdag_blocks[gidx][sidx] = np.array(cdag_blocks[gidx][sidx])
+
     # save dense creation and annihilation operators
     c_dense = np.zeros((2 * norb, 4 ** norb, 4 ** norb))
     cdag_dense = np.zeros((2 * norb, 4 ** norb, 4 ** norb))
@@ -89,3 +125,5 @@ if __name__ == "__main__":
         ar['H_mat_dense'] = H_mat_perm
         ar['c_dense'] = c_dense
         ar['cdag_dense'] = cdag_dense
+        ar['num_sym_sets'] = num_sym_sets
+        ar['sym_set_labels'] = row_groups
